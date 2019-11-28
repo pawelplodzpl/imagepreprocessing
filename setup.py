e0e885f91673e1adc7363e2338976af0f20eccef
can kurt <can.kurt.aa@gmail.com>
2019-11-28 17:47:08 +0300

    first commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="extprint",
+    name="imagepreprocessing",
     version="0.1.0",
     author="Can Kurt",
     author_email="can.kurt.aa@gmail.com",
