2258abd5ea493158e3c9def39b21cbc8f9fe520e
can kurt <can.kurt.aa@gmail.com>
2020-07-14 17:43:40 +0300

    small change

--- imagepreprocessing/__file_operations.py
+++ imagepreprocessing/__file_operations.py
@@ -10,7 +10,7 @@ def __write_to_file(to_write, file_name, write_mode="w"):
     try:
         with open(file_name,write_mode, encoding='utf-8') as file:
             for item in to_write:
-                file.write(item.__str__())
+                file.write(str(item))
                 file.write("\n")
     except (OSError, IOError) as e:
         print(e)
